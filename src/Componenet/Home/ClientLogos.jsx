a74f59a9c01922672cfa1ec07f5e713b42ef753b
Shazidul islam <shazidulislam910@gmail.com>
2025-11-21 21:07:14 +0800

    create profast brand and services section

--- src/Componenet/Home/ClientLogos.jsx
+++ src/Componenet/Home/ClientLogos.jsx
@@ -15,12 +15,12 @@ export default function ClientLogos() {
   const logos = [Logo1, Logo5, Logo3, Logo4, Logo2, Logo6, Logo7];
 
   return (
-    <section className="w-full py-12 bg-gray-100">
-      <div className="max-w-6xl mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center mb-6">Our Successful Clients</h2>
+    <section className="w-full py-12 ">
+      <div className=" px-4">
+        <h2 className="text-3xl text-[#03373D] font-bold text-center mb-12">Our Successful Clients</h2>
         <Marquee gradient={false} speed={50} direction="left">
           {logos.map((logo, index) => (
-            <div key={index} className="mx-8 flex items-center">
+            <div key={index} className="px-20 flex items-center justify-center">
               <img src={logo} alt={`Client Logo ${index + 1}`} className=" object-contain" />
             </div>
           ))}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Componenet/Home/Banner';
 import OurServices from '../Componenet/Home/OurServices';
 import ClientLogos from '../Componenet/Home/ClientLogos';
+import FeatureCards from '../Componenet/Home/FetureCards';
 
 const Home = () => {
     return (
@@ -10,7 +11,7 @@ const Home = () => {
             <OurServices></OurServices>
             <ClientLogos/>
             <div className="border-t-2 border-dashed border-gray-300 my-8"></div>
-
+              <FeatureCards/>
         </div>
     );
 };
